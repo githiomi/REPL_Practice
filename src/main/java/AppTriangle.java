7f3843663a3132f37e99ea0ead618e896a59216a
Dhosio <danielgithiomi@gmail.com>
2020-04-19 16:37:52 +0300

    "This will add the new Triangle app that returns the type of triangle"

--- src/main/java/AppTriangle.java
+++ src/main/java/AppTriangle.java
@@ -4,7 +4,20 @@ public class AppTriangle{
     public static void main(String[] args){
         Console myConsole = System.console();
 
-        System.out.println("Welcome to DISCOVER TRIANGLES!");
+        System.out.println("--------Welcome to DISCOVER TRIANGLES!---------");
         System.out.println("First we are going to need the 3 sides of your triangle! Enter the length of side");
+        System.out.println("Enter side 1 length: ");
+            int sideOne = Integer.parseInt(myConsole.readLine());
+        System.out.println("Enter side 2 length: ");
+            int sideTwo = Integer.parseInt(myConsole.readLine());
+        System.out.println("enter side 3 length: ");
+            int sideThree = Integer.parseInt(myConsole.readLine());
+
+        Triangle newTriangle = new Triangle(sideOne, sideTwo, sideThree);
+        System.out.println("The sides of your triangle measure: " + newTriangle.mSideOne + ", " + newTriangle.mSideTwo + " and " + newTriangle.mSideThree);
+
+
+        System.out.println("You have a " + newTriangle.triangleType(newTriangle.mSideOne, newTriangle.mSideTwo, newTriangle.mSideThree) + " triangle!");
+
+        }
     }
-}
--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,5 +1,25 @@
 class Triangle{
-    public int sideOne;
-    public int sideTwo;
-    public int sideThree;
+    public int mSideOne;
+    public int mSideTwo;
+    public int mSideThree;
+
+    public Triangle(int sideOne, int sideTwo, int sideThree){
+        mSideOne = sideOne;
+        mSideTwo = sideTwo;
+        mSideThree = sideThree;
+    }
+
+    public String triangleType(int lOne, int lTwo, int lThree){
+        if ( lOne == lTwo && lTwo == lThree && lOne == lThree){
+            return "Equilateral";
+        }
+        if (lOne == lTwo && lOne != lThree || lTwo == lThree && lTwo != lOne){
+            return "Isosceles";
+        }
+
+        if ( lOne != lTwo || lTwo != lThree || lOne != lThree){
+            return "Scalene";
+        }
+        return null;
+    }
 }
